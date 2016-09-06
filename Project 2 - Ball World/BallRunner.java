e45868d26c72d3da33f2d7d49aad69574f589353
jloewen <jloewen445@gmail.com>
2016-09-06 08:46:40 -0700

    Add files via upload

---
+++ Project 2 - Ball World/BallRunner.java
@@ -0,0 +1,13 @@
+
+/**
+ *BallRunner 
+ *Jameson Loewen 
+ *September 6, 2016
+ */
+public class BallRunner
+{
+   public static void run (){
+       BallWorld ballWorld = BallWorld (50, 50);
+    }
+   }
+
