58dd3ada6cbe4e636d981eeba1a349bbdd45effe
jloewen <jloewen445@gmail.com>
2016-09-07 08:49:22 -0700

    Add files via upload

--- Project 2 - Ball World/BallRunner.java
+++ Project 2 - Ball World/BallRunner.java
@@ -7,7 +7,9 @@
 public class BallRunner
 {
    public static void run (){
-       BallWorld ballWorld = BallWorld (50, 50);
-    }
-   }
-
+       BallWorld ballWorld = new BallWorld (200, 200);
+       TGPoint tgPoint = new TGPoint (0,0);
+       BallBot ballBot = new BallBot (ballWorld, tgPoint, 90.0, 30);
+       while (
+}
+}
